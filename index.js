a514f32052f83f67139609ab6d61e08b0e3be705
PHILIP <philophilo037@gmail.com>
2022-08-30 06:25:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json =>
+    {renderBooks(json);
+    });
 }
 
 function renderBooks(books) {
@@ -11,7 +16,6 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
-
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
